7f6582401149957a0703c180e0e35f6645a18839
Choco0914 <heojeongho1992@gmail.com>
2019-06-20 10:01:08 +0900

    Express Core: Middlewares

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000;
@@ -14,6 +18,12 @@ const handleHome = (req, res) => {
 const handleProfile = (req, res) => {
   res.send("Hello from Profile");
 };
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
