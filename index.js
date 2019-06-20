4159a3540de4e0a65d611a9a2effb4c9664e3f24
Choco0914 <heojeongho1992@gmail.com>
2019-06-20 09:13:22 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ const handleListening = () => {
   console.log(`✅ Listening on: http://localhost:${PORT}`);
 };
 
+const handleHome = (req, res) => {
+  res.send("Hello from Home");
+};
+
+const handleProfile = (req, res) => {
+  res.send("Hello from Profile");
+};
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
