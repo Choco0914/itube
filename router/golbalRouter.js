31856a297622e6cbacf797264c5b9caec1605369
jeonghoheo <heojeongho1992@gmail.com>
2019-06-28 11:53:23 +0900

    Log In and User Profile Controller

--- router/golbalRouter.js
+++ router/golbalRouter.js
@@ -2,10 +2,11 @@ import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
 import {
-  login,
   logout,
   postJoin,
-  getJoin
+  getJoin,
+  getLogin,
+  postLogin
 } from "../controllers/userController";
 
 const globalRouter = express.Router();
@@ -14,7 +15,8 @@ globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
-globalRouter.get(routes.login, login);
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
